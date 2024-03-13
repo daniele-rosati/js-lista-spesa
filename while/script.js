8c04ee217ea7e4b3bd3b9c272f561314592a63cc
daniele-rosati <drosati151@gmail.com>
2024-03-13 18:33:35 +0100

    struttuta while (con modello for )

---
+++ while/script.js
@@ -0,0 +1,40 @@
+// Data una lista della spesa:
+// const list = [
+//     'Latte',
+//     'Cacao',
+//     'Insalata',
+//     'Carote',
+//     'Pomodori',
+//     'Piatti pronti'
+// ];
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+// Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
+
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+let divSpesa =document.getElementById("spesa");
+
+
+// for (let i=0; i < list.length; i++){
+//     // console.log(list[i]);
+//     divSpesa.innerHTML += list[i] + "<br>";
+// }
+
+// stampa con for degli elementi
+
+let i=0;
+while (i < list.length - 1){
+    i++;
+    divSpesa.innerHTML += list[i] + "<br>";
+}
